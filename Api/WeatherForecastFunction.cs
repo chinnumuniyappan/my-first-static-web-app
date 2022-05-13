4902936a6731f00b935d1d7e4090421fd2d73d5e
chinnumuniyappan <chinnu.muniyappan@syncfusion.com>
2022-05-13 20:25:29 +0530

    PDF sample added.

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -7,6 +7,15 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
 using BlazorApp.Shared;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using System.IO;
+using System.Net.Http;
+using System.Net;
+using Syncfusion.Drawing;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
 
 namespace BlazorApp.Api
 {
@@ -32,22 +41,44 @@ namespace BlazorApp.Api
             return summary;
         }
 
+
         [FunctionName("WeatherForecast")]
-        public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<string> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req, TraceWriter log)
         {
-            var randomNumber = new Random();
-            var temp = 0;
+            //var randomNumber = new Random();
+            //var temp = 0;
 
-            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = temp = randomNumber.Next(-20, 55),
-                Summary = GetSummary(temp)
-            }).ToArray();
+            //var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = temp = randomNumber.Next(-20, 55),
+            //    Summary = GetSummary(temp)
+            //}).ToArray();
 
-            return new OkObjectResult(result);
+            //return new OkObjectResult(result);
+
+            PdfDocument document = new PdfDocument();
+            //Add a page to the document.
+            PdfPage page = document.Pages.Add();
+            //Create PDF graphics for the page.
+            PdfGraphics graphics = page.Graphics;
+            //Set the standard font. 
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+            //Draw the text.
+            graphics.DrawString("Hello World!!!", font, PdfBrushes.Black, new PointF(0, 0));
+            MemoryStream ms = new MemoryStream();
+            //Save the PDF document  
+            document.Save(ms);
+            ms.Position = 0;
+            return Convert.ToBase64String(ms.ToArray());
+            //HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            //response.Content = new ByteArrayContent(ms.ToArray());
+            //response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            //{
+            //    FileName = "PDFDocument.pdf"
+            //};
+            //response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
+            //return response;
         }
     }
 }
